90ca9f0f712c2d9ab2dbd33ca1c4b403128288dc
Felipe Sauer <120697114+felipesauer@users.noreply.github.com>
2023-02-02 09:15:51 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ export default {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ["<rootDir>/src/modules/**/services/*.ts"],
+    collectCoverageFrom: [],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
